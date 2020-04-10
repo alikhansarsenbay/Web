c90d9ab7151514e596e3140e8eadcf6452eeb4f4
alikhansarsenbay <sarsenbai_alihan@mail.ru>
2020-04-10 12:10:18 +0600

    Final project

---
+++ webp/group-project/src/app/collection.ts
@@ -0,0 +1,48 @@
+import { Product } from "./product";
+
+export const Collection: Product[] = [
+    {
+        id: 21,
+        name: 'Jordan 23 Engineered',
+        price: 125,
+        description: "The Jordan 23 Engineered Pullover captures the swagger and influence of modern streetwear. Made from transparent spacer mesh material with sleek nylon overlays, it's relaxed and easy to layer.",
+        rating: 0,
+        img: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-35aa8b00-e3d4-450c-9038-4377a3b19029/jordan-23-engineered-mens-spacer-mesh-pullover-t3g2P7.jpg',
+        url: 'https://www.nike.com/t/jordan-23-engineered-spacer-mesh-pullover-t3g2P7/CJ6063-010',
+        category: 'Collection',
+        category_id: 4,
+    },
+    {
+        id: 22,
+        name: "Jordan: Men's Utility Shorts",
+        price: 90,
+        description: "Cargo functionality meets boutique fashionability. The Jordan 23 Engineered Utility Shorts are made with smooth, ultralight woven fabric. They feature an assortment of zippered and flap pockets to carry essentials.",
+        rating: 0,
+        img: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-bc370304-3be8-43ec-98fb-2ad0111e9cb6/jordan-23-engineered-mens-utility-shorts-mLKJlR.jpg',
+        url: 'https://www.nike.com/t/jordan-23-engineered-mens-utility-shorts-mLKJlR/CN7298-446',
+        category: 'Collection',
+        category_id: 4,
+    },
+    {
+        id: 23,
+        name: "Jordan: Men's Crew",
+        price: 40,
+        description: "The Jordan 23 Engineered Crew is made from lightweight knit fabric that's washed for a soft, comfortable feel. Sweat-wicking technology helps keep you dry.",
+        rating: 0,
+        img: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-2bc65540-438e-480e-8844-753e0701d720/jordan-23-engineered-mens-crew-4mFgvs.jpg',
+        url: 'https://www.nike.com/t/jordan-23-engineered-mens-crew-4mFgvs/CJ6232-010',
+        category: 'Collection',
+        category_id: 4,
+    },
+    {
+        id: 24,
+        name: "Jordan: Men's Spacer Mesh Vest",
+        price: 100,
+        description: "The Jordan 23 Engineered Spacer Mesh Vest takes the look of modern utility in a bold and futuristic direction. Structured spacer mesh material gives it a textural feel and a transparent look. Utility pockets keep it functional.",
+        rating: 0,
+        img: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-e3c1d553-bc53-4752-a6c8-99ce3de78941/jordan-23-engineered-mens-spacer-mesh-vest-FQQSTj.jpg',
+        url: 'https://www.nike.com/t/jordan-23-engineered-mens-spacer-mesh-vest-FQQSTj/CN4584-010',
+        category: 'Collection',
+        category_id: 4,
+    },
+]
